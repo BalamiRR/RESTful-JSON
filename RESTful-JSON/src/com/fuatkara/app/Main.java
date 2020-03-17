3c6b96e38b11437cbd17e94bc4c5df2af6049746
Fuat Kara <fuatkara.95@gmail.com>
2020-03-17 11:03:58 +0100

    Update

--- RESTful-JSON/src/com/fuatkara/app/Main.java
+++ RESTful-JSON/src/com/fuatkara/app/Main.java
@@ -1,10 +1,11 @@
 package com.fuatkara.app;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
-
 
 public class Main {
 	public static void main(String[] args) {
@@ -13,18 +14,15 @@ public class Main {
 		String surname = "Kara";
 		
 		try {
-			URL url = new URL("http://localhost:8080/RESTful-JSON/user/information"+ id + "/" + name + "/" + surname);
+			URL url = new URL("http://localhost:8080/RESTful-JSONServer/rest/user/information"+ id + "/" + name + "/" + surname);
 			
 			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-			 
-			connection.setRequestMethod("GET");  // Burada diyoruz ki yukaridaki adresle ne yapicaz,bizde diyoruz ki 
-												//GET yapicaz, postman'deki gibi
-			connection.setRequestProperty("Accept", "application/xml"); //buraya application/json yazsak json gelecek.
+			connection.setRequestMethod("GET");  // Burada diyoruz ki yukaridaki adresle ne yapicaz,bizde diyoruz kim,GET yapicaz, postman'deki gibi
+			connection.setRequestProperty("Accept", "application/json"+";charset=utf-8"); //buraya application/json yazsak json gelecek.
 			
 			if(connection.getResponseCode() != 200) {
 				throw new RuntimeException("FUAT HTTP CODE :" + connection.getResponseCode());
 			}
-			
 			//Birseyi buffer'lamak bellekte onlari biriktiriyorsun sonra kullaniyorsun.
 			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
 			String result = bufferedReader.readLine();
@@ -32,9 +30,11 @@ public class Main {
 			
 			connection.disconnect();
 			
-		}catch (Exception e) {
+		}catch (MalformedURLException e) {
 			System.err.println("ERRROROROR" + e);    //err.println
 			e.printStackTrace(); 					//ve buda tam hatanin aciklamisni gosteriyor
+		}catch(IOException e){
+			e.printStackTrace();
 		}
 	}
 }
--- RESTful-JSON/src/com/fuatkara/service/UserService.java
+++ RESTful-JSON/src/com/fuatkara/service/UserService.java
@@ -6,7 +6,6 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-
 import com.fuatkara.model.User;
 
 @Path("/user")    //Bu class disariya acilan bir class demek cunku path kullandik diger siniflarda yok.
@@ -24,6 +23,21 @@ public class UserService {
 													//Birde int yaptik cunku biz bu idParameter'i java degiskenine atayacagiz, o da int id'dir. 
 		User userObject = new User();
 		
+		userObject.setUserId(Id);
+		userObject.setUserName(name);
+		userObject.setUserLastName(surname);
+		
+		return Response.status(200).entity(userObject).build();
+	}
+	
+	@GET
+	@Path("/information/{id}/{name}/{surname}")	 
+	public Response getUserInformation(
+			@PathParam("Id") Integer Id,
+			@PathParam("name") String name,
+			@PathParam("surname") String surname){ 
+		
+		User userObject = new User();
 		userObject.setUserId(Id);
 		userObject.setUserName(name);
 		userObject.setUserLastName(surname);
@@ -51,9 +65,9 @@ public class UserService {
 	}
 	
 	
-		//http://localhost:8080/RESTful-JSON/user/information/1/Balamir/Kizil
+		//http://localhost:8080/RESTful-JSON/rest/user/information/1/Balamir/Kizil
 		@GET     
-		@Path("/information/{id}/{name}/{surname}.json")		
+		@Path("/information/{id}/{name}/{surname}")		
 		@Produces({MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML})  //Burda bize ilk olarak Application_JSON gelir. 
 		public Response getUserInformationJsonXml(						   // Cunku ilk parametre
 				@PathParam("Id") Integer Id,
@@ -69,18 +83,18 @@ public class UserService {
 			return Response.status(200).entity(userObject).build();
 		}
 		
-//		//http://localhost:8080/RESTful-JSON/user/information/1
-//		@GET     //Bu classa @GET annotation i yaparak ulasabiliriz.
-//		@Path("/information/{idParameter}")							
-//		public Response getUserInformation( @PathParam("idParameter") Integer id){  
-//			
-//			User userObject = new User();
-//			userObject.setUserId(23);
-//			userObject.setUserName("Sabahattin");
-//			userObject.setUserLastName("Kavak");
-//			
-//			return Response.status(200).entity(userObject).build();
-//		
-//		
-//	}
+		//http://localhost:8080/RESTful-JSON/user/information/1
+		@GET     //Bu classa @GET annotation i yaparak ulasabiliriz.
+		@Path("/information/{idParameter}")							
+		public Response getUserInformation( @PathParam("idParameter") Integer id){  
+			
+			User userObject = new User();
+			userObject.setUserId(23);
+			userObject.setUserName("Sabahattin");
+			userObject.setUserLastName("Kavak");
+			
+			return Response.status(200).entity(userObject).build();
+		
+		
+	}
 }
